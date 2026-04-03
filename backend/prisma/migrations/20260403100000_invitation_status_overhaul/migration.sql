77121b411484cf10c219673fbf95fa6fe2c9317d
Ha Pham <hapham.dev@gmail.com>
2026-04-03 16:41:44 +0700

    Merge database migrations

--- backend/prisma/migrations/20260403100000_invitation_status_overhaul/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterEnum
-ALTER TYPE "InvitationStatus" ADD VALUE IF NOT EXISTS 'initiated';
-ALTER TYPE "InvitationStatus" ADD VALUE IF NOT EXISTS 'sending';
-ALTER TYPE "InvitationStatus" ADD VALUE IF NOT EXISTS 'sent';
